8129ce824502c9fa332ff2859674d94c457a3b7d
SgfKrc <2170725694@qq.com>
2025-11-17 22:05:10 +0800

    Delete frontend/community directory

--- frontend/community/src/api/report.ts
+++
@@ -1,7 +0,0 @@
-import request from '@/utils/request';
-import type { Report } from '@/types';
-
-// 提交举报
-export const submitReport = (data: Report) => {
-  return request.post('/reports', data);
-};
